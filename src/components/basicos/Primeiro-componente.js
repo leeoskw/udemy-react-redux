d836e35652d051f07854f39b59bd90f70b9227d1
Leonardo <leeo_skw@hotmail.com>
2022-05-15 19:59:25 -0300

    Criação de componente; utilização de JSX; import de CSS externo

---
+++ src/components/basicos/Primeiro-componente.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Primeiro() {
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 
+import './index.css'
+
+import Primeiro from './components/basicos/Primeiro-componente'
 
 const elemento =  document.getElementById('root')
-ReactDOM.render('Olá mundo!', elemento)
+// ReactDOM.render('Olá mundo!', elemento)
+// ReactDOM.render(<div>Hello, world!</div>, elemento)
+ReactDOM.render(
+<div>
+    <Primeiro></Primeiro> <br></br>
+    <Primeiro></Primeiro>
+</div>
+, elemento) // utilizando um componente criado
