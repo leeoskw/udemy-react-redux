5e8479f560586677dbde17f50d1f32e01aee9f26
Leonardo <leeo_skw@hotmail.com>
2022-05-15 23:15:47 -0300

    Adição de css (flex) aos cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,32 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/layout/Card'
 import Primeiro from './components/basicos/Primeiro-componente'
+import './App.css'
 
 export default (props) => {
     return (
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React</h1>
 
-            <Card titulo="#04 - Desafio Aleatorio">
-                <Aleatorio minimo={1} maximo={40}></Aleatorio>
-            </Card>
+            <div className='Exercicios'>
+                <Card titulo="#04 - Desafio Aleatorio">
+                    <Aleatorio minimo={1} maximo={40}></Aleatorio>
+                </Card>
 
-            <Card titulo="#03 - Fragmento">
-                <Fragmento></Fragmento>
-            </Card>
+                <Card titulo="#03 - Fragmento">
+                    <Fragmento></Fragmento>
+                </Card>
 
-            <Card titulo="#02 - Passagem de Parâmetros">
-                <ComParametro titulo="Meu Titulo" subtitulo="Meu Subtitulo"></ComParametro>
-            </Card>
+                <Card titulo="#02 - Passagem de Parâmetros">
+                    <ComParametro titulo="Meu Titulo" subtitulo="Meu Subtitulo"></ComParametro>
+                </Card>
 
-            <Card titulo = "#01 - Primeiro Componente">
-                <Primeiro></Primeiro>
-            </Card>
+                <Card titulo="#01 - Primeiro Componente">
+                    <Primeiro></Primeiro>
+                </Card>
+            </div>
+
+
 
         </div>
     )
